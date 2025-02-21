138aa5728119298be04fa1e1739a6a2e111b0233
Nehu <niahrik@gmail.com>
2025-02-21 18:29:28 +0530

    Day-4

---
+++ Week-2/day-5/Script/script.js
@@ -0,0 +1,39 @@
+let api_url="https://jsonplaceholder.typicode.com/users";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getRandomComputerResult(){
+//     const output=Options[Math.floor(Math.random() * options.length)];
+//     return output
+// }
+// console.log(getRandomComputerResult());
+
+
+
+
+// let fetdata=fetch("https://jsonplaceholder.typicode.com/users");
+// fetdata.then((response)=>{
+//     if (response.ok){
+//         return response.json();
+//     }else{
+//         throw new Error("network response error");
+//     }
+// })
+// fetdata.then(data =>{
+//     console.log(data);
+//     displayData(data)
+// })
+// fetdata.catch((error)=>{
+//     console.log.error("fetch error",error)
+// })
